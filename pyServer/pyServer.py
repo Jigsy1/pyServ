0f061c1fe92d38b87b4d9c9435ed3147dcefb56f
Jigsy <34282672+Jigsy1@users.noreply.github.com>
2023-01-24 09:11:32 +0000

    Add files via upload
    Added JupeServ.

--- pyServer/pyServer.py
+++ pyServer/pyServer.py
@@ -2,8 +2,20 @@
 #
 # This script will not work unless you have a C:line[1] on an ircu based IRCd. (http://ircd.bircd.org/)
 #
-# Sadly, unlike the mSL jupe I wrote, there isn't any way for you to be able to send raw commands natively due to the while loop.
-# However, I might update this to allow a juped user that allows raw commands to be sent.
+# Example commands (using JupeServ):
+# -----------------------------------
+# ?raw AA P #channel :Hello! - Makes the server itself say "Hello!" in #channel.
+# ?raw AA M #channel +v ATAAx - Gives a +v to the user who has the numeric of ATAAx[2] on #channel.
+# ?raw AA N FBI 1 1674548945 noreply fbi.gov +iko B]AAAB AAAAA :FBI
+#
+# ...will create the user FBI!noreply@fbi.gov with usermodes +iko and an ip of 127.0.0.1 (B]AAAB) as user AAAAA[3] on jupe server.
+# NOTE: Creating AAAAA on top of AAAAA will[4] result in a SQUIT, so you might want to keep track of who has been created if you plan on doing this.
+#
+# ?raw AAAAA J #channel - Will make our newly created "FBI" user join #channel.
+# ?raw AA J #channel - Will cause the server to SQUIT because servers cannot join channels. :P
+# ?raw AA I FBI #channel - Will also cause the server to SQUIT because servers cannot invite users apparently.[5]
+#
+# Note: You need to use the nick, not the numeric in order to invite a user.
 #
 # Further reading:
 # --------------------
@@ -15,8 +27,84 @@
 
 # include(s):
 
+import hashlib
 import socket
 import time
+
+
+# JupeServ Command(s):
+
+def parse_jupeserv_command(command, data):
+    JUPESERV_COMMANDS[command](data)
+
+def parse_jupeserv_auth(data):
+    # AUTH <account> <password>
+
+    if is_channel(data[2]) == True:
+        sendString(data[0], JUPESERV_ERR_NOTPUBLIC)
+        return
+    if data[2] != JUPESERV_SECURE:
+        sendString(data[0], JUPESERV_ERR_USESECURE)
+        return
+    if is_array(data, 5) == False:
+        sendString(data[0], JUPESERV_ERR_NEEDMOREARGS)
+        return
+    if data[0] in JUPESERV_AUTHED:
+        sendString(data[0], "You are already authed.")
+        return
+    account = data[4].lower()
+    if account not in JUPESERV_USERS:
+        sendString(data[0], "That account does not exist.")
+        return
+    password = data[5].strip(NEW_LINES)
+    password = "(" + str(JUPESERV_SALT) + ":" + password + ")"
+    password = password.encode("UTF-8")
+    output = hashlib.sha512(password).hexdigest()
+    if JUPESERV_USERS[account] != output:
+        sendString(data[0], "That password is incorrect.")
+        return
+    JUPESERV_AUTHED.append(data[0])
+    sendString(data[0], "You are now authed.")
+
+def parse_jupeserv_help(data):
+    # HELP
+
+    sendString(data[0], "The following command(s) are available.")
+    sendString(data[0], "----------------------------------------")
+    for item in JUPESERV_COMMANDS:
+        string = item + " " + JUPESERV_COMMANDS_HELP[item]
+        sendString(data[0], string)
+
+def parse_jupeserv_mkpasswd(data):
+    # MKPASSWD <input>
+
+    if is_channel(data[2]) == True:
+        sendString(data[0], JUPESERV_ERR_NOTPUBLIC)
+        return
+    if data[2] != JUPESERV_SECURE:
+        sendString(data[0], JUPESERV_ERR_USESECURE)
+        return
+    if len(data[3:]) == 1:
+        sendString(data[0], JUPESERV_ERR_NEEDMOREARGS)
+        return
+    userInput = data[4].strip(NEW_LINES)
+    userInput = "(" + str(JUPESERV_SALT) + ":" + userInput + ")"
+    userInput = userInput.encode("UTF-8")
+    output = hashlib.sha512(userInput).hexdigest()
+    sendString(data[0], "Hash: " + output)
+
+def parse_jupeserv_raw(data):
+    # RAW <args>
+
+    if data[0] not in JUPESERV_AUTHED:
+        sendString(data[0], JUPESERV_ERR_ACCESSDENIED)
+        return
+    if len(data[3:]) == 1:
+        sendString(data[0], JUPESERV_ERR_NEEDMOREARGS)
+        return
+    sendLine = " ".join(data[4:])
+    sendRaw("{}{}".format(sendLine, NEW_LINES))
+    sendString(data[0], JUPESERV_RPL_DONE)
 
 
 # P10/Server Function(s):
@@ -25,44 +113,75 @@ def parse_P10_command(command, data):
     P10_COMMANDS[command](data)
 
 def parse_P10_info(data):
-    # `-> <numeric> <F|INFO> <server numeric>
+    # <numeric> <F|INFO> <server numeric>
 
     data = data.split(" ")
-    sendRaw("{} 371 {} :{}{}".format(JUPE_BASENUM, data[0], JUPE_NAME, NEW_LINES))
-    sendRaw("{} 371 {} :{}{}".format(JUPE_BASENUM, data[0], JUPE_INFO, NEW_LINES))
-    sendRaw("{} 374 {} :End of /INFO list.{}".format(JUPE_BASENUM, data[0], NEW_LINES))
+    sendRaw("{} 371 {} :{}{}".format(JUPE_NUMERIC, data[0], JUPE_NAME, NEW_LINES))
+    sendRaw("{} 371 {} :{}{}".format(JUPE_NUMERIC, data[0], JUPE_INFO, NEW_LINES))
+    sendRaw("{} 374 {} :{}{}".format(JUPE_NUMERIC, data[0], RPL_ENDOFINFO, NEW_LINES))
+
+def parse_P10_kill(data):
+    # <numeric> <D|KILL> <numeric> :<reason>
+
+    line = data.split(" ")
+    if line[2] in JUPESERV_AUTHED:
+        JUPESERV_AUTHED.remove(line[2])
 
 def parse_P10_motd(data):
-    # `-> <numeric> MO[TD] <server numeric>
+    # <numeric> MO[TD] <server numeric>
 
-    sendRaw("{} 422 {} :{}{}".format(JUPE_BASENUM, data.split(" ")[0], ERR_NOMOTD, NEW_LINES))
+    sendRaw("{} 422 {} :{}{}".format(JUPE_NUMERIC, data.split(" ")[0], ERR_NOMOTD, NEW_LINES))
 
 def parse_P10_ping(data):
-    # `-> <numeric> <G|PING> [:]<arg>
+    # <numeric> <G|PING> [:]<arg>
 
-    sendRaw("{} Z {}{}".format(JUPE_BASENUM, data.split(" ")[2], NEW_LINES))
+    sendRaw("{} Z {}{}".format(JUPE_NUMERIC, data.split(" ")[2], NEW_LINES))
     # `-> Saying PONG instead of Z should also work; but let's just leave it alone.
 
-def parse_P10_time(data):
-    # `-> <numeric> TI[ME] <server numeric>
+def parse_P10_privmsg(data):
+    # <numeric> P[RIVMSG] <target> :<message>
+    #
+    # Note: Target will either be a channel, numeric, or <bot>@<server>.
 
-    sendRaw("{} 391 {} {}{}".format(JUPE_BASENUM, data.split(" ")[0], RPL_TIME.format(JUPE_NAME, str(int(time.time())), 0, time.strftime("%A %B %d %Y -- %H:%M:%S %z")), NEW_LINES))
+    line = data.split(" ")
+    command = line[3][1:].lower()
+    command = command.strip(NEW_LINES)
+    # ¦-> So, when regular users get raw data on bircd, there isn't any \n on the end of a PRIVMSG line.
+    # ¦-> However, with servers, this is a completely different story. There is an \n appended to the end of the line.
+    # `-> As a result of this annoyance, I'm stripping NEW_LINES from the command name.
+    if line[2] != JUPESERV_NUMERIC and line[2] != JUPESERV_SECURE:
+        if command[0] != JUPESERV_TRIGGER:
+            return
+        command = command[1:]
+    if command not in JUPESERV_COMMANDS:
+        if line[2] == JUPESERV_NUMERIC:
+            sendString(line[0], JUPESERV_ERR_NOSUCHCOMMAND.format(command))
+        return
+    parse_jupeserv_command(command, line)
+
+def parse_P10_quit(data):
+    # <numeric> Q[UIT] :[quit message]
+
+    line = data.split(" ")
+    if line[0] in JUPESERV_AUTHED:
+        JUPESERV_AUTHED.remove(line[0])
+
+def parse_P10_time(data):
+    # <numeric> TI[ME] <server numeric>
+
+    sendRaw("{} 391 {} {}{}".format(JUPE_NUMERIC, data.split(" ")[0], RPL_TIME.format(JUPE_NAME, str(int(time.time())), 0, time.strftime("%A %B %d %Y -- %H:%M:%S %z")), NEW_LINES))
     # `-> 0 is offset. I don't know what to put here, so I'm leaving it as zero.
 
 
 # Function(s):
 
-def mid(string, offset, amount):
-    return string[offset-1:offset+amount-1]
-# `-> Credit where credit is due, this is by Stanton Attree on Stack Overflow.
-
-def base64toint(input):
+def base64toint(numeric):
     """Convert a base64 numeric to a number. E.g. base64toint("Qz]")"""
     o = 0
     x = 1
-    while mid(input, x, 1):
+    while mid(numeric, x, 1):
         o = (o * 64)
-        o = (o + i(mid(input, x, 1)))
+        o = (o + i(mid(numeric, x, 1)))
         x += 1
     return o
 
@@ -70,12 +189,12 @@ def binaryAnd(A, B):
     """Returns A binary and B."""
     return int(A) & int(B)
 
-def i(input):
+def i(arg):
     P10_BASE64 = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789[]"
-    return P10_BASE64.find(input)
+    return P10_BASE64.find(arg)
 
-def ii(input):
-    return mid("ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789[]", (int(input) + 1), 1)
+def ii(arg):
+    return mid("ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789[]", (int(arg) + 1), 1)
 
 def inttobase64(number, padding):
     """Convert a numeric number to base64 with the specified padded length. E.g. inttobase64(482, 2)"""
@@ -87,19 +206,70 @@ def inttobase64(number, padding):
         v = (v / 64)
         c -= 1
     return o
-# [2]
+# [6]
 
-def sendRaw(input):
-    jupe.send(input.encode("UTF-8"))
-    print(input.strip(NEW_LINES))
+def is_array(array, index):
+    """Returns true or false depending on if an index exists within an array."""
+    try:
+        array[index]
+    except IndexError:
+        return False
+    return True
+
+def is_channel(channel):
+    """Returns true or false depending on if the input is a channel."""
+    if channel[0] == "#":
+        return True
+    return False
+
+def mid(string, offset, amount):
+    return string[offset-1:offset+amount-1]
+# `-> Credit where credit is due, this is by Stanton Attree on Stack Overflow.
+
+def sendRaw(data):
+    jupe.send(data.encode("UTF-8"))
+    print(data.strip(NEW_LINES))
+
+def sendString(numeric, string):
+    sendRaw("{} {} {} :{}{}".format(JUPESERV_NUMERIC, JUPESERV_REPLY_METHOD, numeric, string, NEW_LINES))
 
 
 # define(s):
 
-JUPE_FLAGS = "+"
+JUPESERV_AUTHED = []
+# `-> Leave this blank. The bot will handle this. (NOTE: Not currently handled by SQ[UIT].)
+JUPESERV_CHAN = "#JupeServ"
+JUPESERV_BOT = "JupeServ"
+# [7]
+JUPESERV_TRIGGER = "?"
+JUPESERV_ENABLED = 1
+JUPESERV_REPLY_METHOD = "O"
+# `-> This is a NOTICE (P10 = O). If you want PRIVMSG, use P.
+JUPESERV_SALT = "changeme"
+JUPESERV_USERS = {
+    "changeme": "98d21743a2586d3eda1f45c097f7239c5ab9bd770d515f91d510972b1e4d7925c5f3e72a46b0bb1f68f0c43efb2bd5635b7ff1391d16121af4b44896609a6f0d"
+}
+# ¦-> Names and sha512 passwords must be in lowercase. To use this, connect to the IRCd and type: /msg <bot>@<server> MKPASSWD <password>
+# `-> The hash is unsalted 128 character junk input, by the way.
+
+JUPESERV_COMMANDS = {
+    "auth": parse_jupeserv_auth,
+    "help": parse_jupeserv_help,
+    "mkpasswd": parse_jupeserv_mkpasswd,
+    "raw": parse_jupeserv_raw
+}
+JUPESERV_COMMANDS_HELP = {
+    "auth": "- AUTH <account> <password> (Auth to {} to use RAW.)".format(JUPESERV_BOT),
+    "help": "- HELP (Lists all available commands.)",
+    "mkpasswd": "- MKPASSWD <input> (Encrypts a string for password use.)",
+    "raw": "- RAW <args> (Send raw args. This requires knowledge of what you're doing.)"
+}
+# `-> /msg <bot> HELP
+
+JUPE_FLAGS = "+s"
 # `-> Append flags - which in this case is basically just 6 or s (or both) - with +. If no flags are specified, just leave it as: +
 JUPE_INFO = "A jupe server for ircu P10 protocol in Python."
-JUPE_NUMERIC = 0
+JUPE_INT_NUMERIC = 0
 # `-> The numeric of our server. Limited between 0 and 4095.
 JUPE_PASSWORD = "changeme"
 # `-> Plaintext password.
@@ -111,41 +281,69 @@ JUPE_NAME = "changeme.localhost"
 # `-> The name of our server.
 
 P10_COMMANDS = {
+    "D": parse_P10_kill,
     "INFO": parse_P10_info,
     "F": parse_P10_info,
     "G": parse_P10_ping,
+    "KILL": parse_P10_kill,
     "MO": parse_P10_motd,
     "MOTD": parse_P10_motd,
+    "P": parse_P10_privmsg,
     "PING": parse_P10_ping,
+    "PRIVMSG": parse_P10_privmsg,
+    "Q": parse_P10_quit,
+    "QUIT": parse_P10_quit,
     "TI": parse_P10_time,
     "TIME": parse_P10_time
 }
-# `-> Cover both LONG and SHORT names.
+# `-> Cover both LONG and S[HORT] names since both are acceptable.
+
+JUPE_NUMERIC = inttobase64(JUPE_INT_NUMERIC, 2)
+# `-> Convert our numeric to the base64 name for use in specific sendRaw() responses.
+JUPESERV_NUMERIC = JUPE_NUMERIC + "AAA"
+JUPESERV_SECURE = JUPESERV_BOT + "@" + JUPE_NAME
+# `-> For /msg <bot>@<server> ...
+JUPE_EPOCH = str(int(time.time()))
 
 ERR_NOMOTD = "MOTD File is missing"
+RPL_ENDOFINFO = "End of /INFO list."
 RPL_TIME = "{} {} {} :{}"
+
+JUPESERV_ERR_ACCESSDENIED = "You do not have permission to use this command."
+JUPESERV_ERR_NEEDMOREARGS = "Insufficient parameters."
+JUPESERV_ERR_NOSUCHCOMMAND = "The command [{}] does not exist."
+JUPESERV_ERR_NOTPUBLIC = "Security violation. You may not use this command publicly."
+JUPESERV_ERR_USESECURE = "Security violation. Please /msg {} <command> [args] instead.".format(JUPESERV_SECURE)
+JUPESERV_RPL_DONE = "Done."
 
 NEW_LINES = "\r\n"
 
 jupe = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 jupe.connect((JUPE_SERVER, JUPE_PORT))
 
-JUPE_BASENUM = inttobase64(JUPE_NUMERIC, 2)
-# `-> Convert our numeric to the base64 name for use in specific sendRaw() responses.
-JUPE_EPOCH = str(int(time.time()))
-
-
 # Core:
 
 def main():
     sendRaw("PASS :{}{}".format(JUPE_PASSWORD, NEW_LINES))
     # `-> PASS must _ALWAYS_ come first.
-    sendRaw("SERVER {} 1 {} {} J10 {}]]] {} :{}{}".format(JUPE_NAME, JUPE_EPOCH, JUPE_EPOCH, JUPE_BASENUM, JUPE_FLAGS, JUPE_INFO, NEW_LINES))
+    sendRaw("SERVER {} 1 {} {} J10 {}]]] {} :{}{}".format(JUPE_NAME, JUPE_EPOCH, JUPE_EPOCH, JUPE_NUMERIC, JUPE_FLAGS, JUPE_INFO, NEW_LINES))
     # ¦-> SERVER <our server name> <hop count> <connection time> <link time> <protocol> <our server numeric><max users as numeric> [+flags] :<description>
+    # ¦
     # ¦-> We're joining the server, so we use J10, not P10. And ]]] means the maximum number of users allowed. (262,143)
     # ¦-> Flags may or may not being used here; but +s would mean Services. E.g. ... J10 SV]]] +s :IRC Services
     # `-> NOTE: In case of adding a new server post END_OF_BURST, flags must be specified! Even if it's just + otherwise the server _WILL_ SQUIT.
-    sendRaw("{} EB{}".format(JUPE_BASENUM, NEW_LINES))
+    if JUPESERV_ENABLED == 1:
+        sendRaw("{} N {} 1 {} {} {} +iko {} {} :{}{}".format(JUPE_NUMERIC, JUPESERV_BOT, JUPE_EPOCH, JUPESERV_BOT, JUPE_NAME, inttobase64(2130706433, 6), JUPESERV_NUMERIC, JUPESERV_BOT, NEW_LINES))
+        # ¦-> <numeric> N[ICK] <hop count> <timestamp> <user> <host> +<modes> [account|fakehost] [account|fakehost] <ip as base64 numeric> <numeric> :<real name>
+        # ¦
+        # `-> Here, we create a user on the server.
+        sendRaw("{} B {} {} +inst {}:o{}".format(JUPE_NUMERIC, JUPESERV_CHAN, JUPE_EPOCH, JUPESERV_NUMERIC, NEW_LINES))
+        # ¦-> Initial:  <numeric> B[URST] <#channel> <timestamp> <modes> [args] <numeric[:status>[,<numeric[:status>],...]] [:%n!u@h n!u@h ...]
+        # ¦-> Overflow: <numeric> B[URST] <#channel> <timestamp> [{numeric[:status}[,{numeric[:status}],...]] [:%n!u@h n!u@h ...]
+        # ¦
+        # ¦-> "Create" a channel upon connection. This line pretends that the channel already exists, and we're just BURSTing that information.
+        # `-> However, if the channel already exists, the older timestamp takes priority. (So in that case the bot won't be opped.)
+    sendRaw("{} EB{}".format(JUPE_NUMERIC, NEW_LINES))
     # `-> END_OF_BURST
 
     while 1:
@@ -165,13 +363,23 @@ def main():
                 parse_P10_command(newLine[0], line)
     jupe.close()
     print("Socket closed.\n")
+    # JUPESERV_AUTHED.clear()
+    # `-> Probably redundant.
 
 if __name__ == "__main__":
     main()
 
 
 # Footnote(s):
+# ---------------
 # [1]: C:127.0.0.1:changeme:changeme.localhost::0 in an ircd.conf
-# [2]: Thanks to Dave (Codex` / Hero_Number_Zero) for once sharing the numeric/P10 conversion code with me nearly a decade ago. (Ported to Python.)
+# [2]: Example only. Factors like the numeric of the main server (AA, AB, etc.) will change this. (AAAAx, ABAAx, ACAAx, etc.)
+#      You'll be able to tell what the numeric of the main server is from B information on linking. (Or hopefully from doing /map.)
+# [3]: This example is operating under the assumption that you don't fiddle with the numeric number above. (0)
+# [4]: AAAAA on top of AAAAA will result in a numeric collision (thus SQUIT). However, making "AAAAA Q :Quit." first is fine.
+# [5]: I honestly fail to see how a server inviting a user to a channel is a problem. (They can change modes, kick users, talk,
+#      all without being on the channel. So why can't they invite anybody?)
+# [6]: Thanks to Dave (Codex` / Hero_Number_Zero) for once sharing the numeric/P10 conversion code with me nearly a decade ago. (Ported to Python.)
+# [7]: If you're worried about the name being used, use a U:line in the ircd.conf. E.g. U:changeme.localhost:JupeServ:
 #
 # EOF
